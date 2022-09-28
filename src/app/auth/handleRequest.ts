9ad9d60a870744c1c7fe98f8b266533b0c4992a0
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-09-28 14:59:51 +0200

    chore: linting

--- src/app/auth/handleRequest.ts
+++ src/app/auth/handleRequest.ts
@@ -2,42 +2,42 @@ import { Session } from '@gemeentenijmegen/session';
 import { OpenIDConnect } from './shared/OpenIDConnect';
 
 function redirectResponse(location: string, code = 302, cookies?: string[]) {
-    return {
-        'statusCode': code,
-        'body': '',
-        'headers': {
-            'Location': location
-        },
-        'cookies': cookies
-    };
+  return {
+    statusCode: code,
+    body: '',
+    headers: {
+      Location: location,
+    },
+    cookies: cookies,
+  };
 }
 
 export async function handleRequest(cookies: any, queryStringParamCode: string, queryStringParamState: string, dynamoDBClient: any): Promise<any> {
-    let session = new Session(cookies, dynamoDBClient);
-    await session.init();
-    if (session.sessionId === false) {
-        return redirectResponse('/login');
+  let session = new Session(cookies, dynamoDBClient);
+  await session.init();
+  if (session.sessionId === false) {
+    return redirectResponse('/login');
+  }
+  const state = session.getValue('state');
+  const contact_id = session.getValue('contact_id');
+  const OIDC = new OpenIDConnect();
+  try {
+    const tokenSet = await OIDC.authorize(queryStringParamCode, state, queryStringParamState, queryStringParamState);
+    if (tokenSet) {
+      await session.createSession({
+        loggedin: { BOOL: true },
+        access_token: { S: tokenSet.access_token },
+        refresh_token: { S: tokenSet.refresh_token },
+        expires_in: { N: `${tokenSet.expires_in}` },
+      });
+    } else {
+      return { statusCode: 500 };
     }
-    const state = session.getValue('state');
-    const contact_id = session.getValue('contact_id');
-    const OIDC = new OpenIDConnect(); 
-    try {
-        const tokenSet = await OIDC.authorize(queryStringParamCode, state, queryStringParamState, queryStringParamState);    
-        if (tokenSet) {
-            await session.createSession({ 
-                loggedin: { BOOL: true },
-                access_token: { S: tokenSet.access_token },
-                refresh_token: { S: tokenSet.refresh_token },
-                expires_in: { N: `${tokenSet.expires_in}` },
-            });
-        } else {
-            return { 'statusCode': 500 }
-        }
-    } catch (error: any) {
-        console.debug('test2', error);
-        console.error(error.message);
-        return redirectResponse('/login');
-    }
-    const url = contact_id ? `/?contact_id=${contact_id}` : '/';
-    return redirectResponse(url, 302, [session.getCookie()]);
+  } catch (error: any) {
+    console.debug('test2', error);
+    console.error(error.message);
+    return redirectResponse('/login');
+  }
+  const url = contact_id ? `/?contact_id=${contact_id}` : '/';
+  return redirectResponse(url, 302, [session.getCookie()]);
 }
--- src/app/home/homeRequestHandler.ts
+++ src/app/home/homeRequestHandler.ts
@@ -1,47 +1,47 @@
-import { render } from './shared/render';
 import { Session } from '@gemeentenijmegen/session';
+import { render } from './shared/render';
 
 function redirectResponse(location: string, code = 302) {
-    return {
-        'statusCode': code,
-        'body': '',
-        'headers': { 
-            'Location': location
-        }
-    }
+  return {
+    statusCode: code,
+    body: '',
+    headers: {
+      Location: location,
+    },
+  };
 }
 
 export async function homeRequestHandler(params: any, apiClient: any, dynamoDBClient: any): Promise<any> {
-    let session = new Session(params.cookies, dynamoDBClient);
-    await session.init();
-    if (session.isLoggedIn() == true) {
-        return await handleLoggedinRequest(session, apiClient, params.contact_id);
-    }
-    return redirectResponse(`/login?contact_id=${params.contact_id}`);
+  let session = new Session(params.cookies, dynamoDBClient);
+  await session.init();
+  if (session.isLoggedIn() == true) {
+    return handleLoggedinRequest(session, apiClient, params.contact_id);
+  }
+  return redirectResponse(`/login?contact_id=${params.contact_id}`);
 }
 
 async function handleLoggedinRequest(session: any, _apiClient: any, _contact_id: string) {
-    // const bsn = session.getValue('bsn');
-    const data = {
-        title: 'overzicht',
-        shownav: true
-    };
+  // const bsn = session.getValue('bsn');
+  const data = {
+    title: 'overzicht',
+    shownav: true,
+  };
 
-    // render page
-    const html = await render(data, __dirname + '/templates/home.mustache', {
-        'header': `${__dirname}/shared/header.mustache`,
-        'footer': `${__dirname}/shared/footer.mustache`
-    });
-    const response = {
-        'statusCode': 200,
-        'body': html,
-        'headers': {
-            'Content-type': 'text/html'
-        },
-        'cookies': [
-            session.getCookie(),
-        ]
-    };
-    return response;
+  // render page
+  const html = await render(data, __dirname + '/templates/home.mustache', {
+    header: `${__dirname}/shared/header.mustache`,
+    footer: `${__dirname}/shared/footer.mustache`,
+  });
+  const response = {
+    statusCode: 200,
+    body: html,
+    headers: {
+      'Content-type': 'text/html',
+    },
+    cookies: [
+      session.getCookie(),
+    ],
+  };
+  return response;
 }
 
