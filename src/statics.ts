4708619b04c640ef1099c23c1790eaccf9a19f8a
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2023-05-05 16:35:52 +0200

    remove cdk prefix

--- src/statics.ts
+++ src/statics.ts
@@ -22,62 +22,62 @@ export abstract class Statics {
    * Route53 Zone ID and name for the zone for the application. decouples stacks to not pass
    * the actual zone between stacks. This param is set by DNSStack and should not be modified after.
    */
-  static readonly ssmZonePath: string = `/cdk/${this.projectName}/zones`;
-  static readonly ssmZoneId: string = `/cdk/${this.projectName}/zones/zone-id`;
-  static readonly ssmZoneName: string = `/cdk/${this.projectName}/zones/zone-name`;
+  static readonly ssmZonePath: string = `/${this.projectName}/zones`;
+  static readonly ssmZoneId: string = `/${this.projectName}/zones/zone-id`;
+  static readonly ssmZoneName: string = `/${this.projectName}/zones/zone-name`;
 
-  static readonly ssmApiGatewayDomain: string = `/cdk/${this.projectName}/apigateway-domain`;
+  static readonly ssmApiGatewayDomain: string = `/${this.projectName}/apigateway-domain`;
 
   /** There seems to be no way to get the required ds record value in the CDK/API */
-  static readonly ssmNijmegenDSRecordValue: string = `/cdk/${this.projectName}/ds-record-value`;
+  static readonly ssmNijmegenDSRecordValue: string = `/${this.projectName}/ds-record-value`;
 
-  static readonly certificatePath: string = `/cdk/${this.projectName}/certificates`;
-  static readonly certificateArn: string = `/cdk/${this.projectName}/certificates/certificate-arn`;
-
-  /**
-   * Authentication URL base, used in auth and login lambda
-   */
-  static readonly ssmAuthUrlBaseParameter: string = `/cdk/${this.projectName}/authUrlBase`;
+  static readonly certificatePath: string = `/${this.projectName}/certificates`;
+  static readonly certificateArn: string = `/${this.projectName}/certificates/certificate-arn`;
 
   /**
    * Authentication URL base, used in auth and login lambda
    */
-  static readonly ssmAuthOrgId: string = `/cdk/${this.projectName}/AuthOrgId`;
+  static readonly ssmAuthUrlBaseParameter: string = `/${this.projectName}/authUrlBase`;
+
+  /**
+   * Authentication URL base, used in auth and login lambda
+   */
+  static readonly ssmAuthOrgId: string = `/${this.projectName}/AuthOrgId`;
 
   /**
    *
     * OpenID Connect client ID (sent in URL as querystring-param, not secret)
     */
-  static readonly ssmOIDCClientID: string = `/cdk/${this.projectName}/authClientID`;
+  static readonly ssmOIDCClientID: string = `/${this.projectName}/authClientID`;
   /**
     * OpenID Connect scope
     */
-  static readonly ssmOIDCScope: string = `/cdk/${this.projectName}/authScope`;
+  static readonly ssmOIDCScope: string = `/${this.projectName}/authScope`;
 
   /**
     * OpenID Connect secret name
     */
-  static readonly secretOIDCClientSecret: string = `/cdk/${this.projectName}/oidc-clientsecret`;
+  static readonly secretOIDCClientSecret: string = `/${this.projectName}/oidc-clientsecret`;
 
   /**
     * Certificate private key for mTLS
     */
-  static readonly secretMTLSPrivateKey: string = `/cdk/${this.projectName}/mtls-privatekey`;
+  static readonly secretMTLSPrivateKey: string = `/${this.projectName}/mtls-privatekey`;
 
   /**
     * Certificate for mTLS
     */
-  static readonly ssmMTLSClientCert: string = `/cdk/${this.projectName}/mtls-clientcert`;
+  static readonly ssmMTLSClientCert: string = `/${this.projectName}/mtls-clientcert`;
 
   /**
      * Root CA for mTLS (PKIO root)
      */
-  static readonly ssmMTLSRootCA: string = `/cdk/${this.projectName}/mtls-rootca`;
+  static readonly ssmMTLSRootCA: string = `/${this.projectName}/mtls-rootca`;
 
-  static readonly ssmBrpApiEndpointUrl: string = `/cdk/${this.projectName}/brp-api-url`;
+  static readonly ssmBrpApiEndpointUrl: string = `/${this.projectName}/brp-api-url`;
 
-  static readonly ssmSessionsTableArn: string = `/cdk/${this.projectName}/sessionstable-arn`;
+  static readonly ssmSessionsTableArn: string = `/${this.projectName}/sessionstable-arn`;
 
-  static readonly ssmDataKeyArn: string = `/cdk/${this.projectName}/kms-datakey-arn`;
+  static readonly ssmDataKeyArn: string = `/${this.projectName}/kms-datakey-arn`;
 
 }
